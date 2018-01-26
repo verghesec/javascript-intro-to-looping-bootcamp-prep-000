7d8779739826eccacb60f11650dbcfbe0073ebba
Cecil Verghese <cecil.verghese@gmail.com>
2018-01-26 04:15:26 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,11 @@
 
-function forLoop() {
+function forLoop(array) {
   for (var i=0; i<25; i++) {
-    do
+    if (i===1) {
+      array.push("I am 1 strange loop");
+    }
+    else {
+      array.push(`I am ${i} strange loops`);
+    }
   }
 }
